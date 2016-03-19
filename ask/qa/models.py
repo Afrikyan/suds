86f68554460c33f3a40aa62040d0f968263212a3
iconductor7 <iconductor7@gmail.com>
2016-03-19 03:16:16 +0300

    mysql.sh3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
 
 class Question(models.Model):
-    title = models.CharField(max_length=50)
-    text = models.CharField(max_length=100)
-    added_at = models.DateField()
-    rating = models.IntegerField()
-    author = models.CharField(max_length=20)
-    likes = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='questions', blank=True)
+    def get_absolute_url(self):
+        return reverse('question', kwargs={"id": self.id})
 
 class Answer(models.Model):
-    text = models.CharField(max_length=100)
-    added_at = models.DateField()
-    question = models.OneToOneField(Question)
-    author = models.CharField(max_length=20)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
