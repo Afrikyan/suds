c6f95d965e942edf100ae9f6291cc654bb2d754f
iconductor7 <iconductor7@gmail.com>
2016-03-22 17:09:22 +0300

    ask django5

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.utils.timezone import now
 
 
 # Create your models here.
@@ -12,15 +13,17 @@ from django.core.urlresolvers import reverse
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True, default=now)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, default=0)
     likes = models.ManyToManyField(User, related_name='questions', blank=True)
     def get_absolute_url(self):
         return reverse('question', kwargs={"id": self.id})
+    def __unicode__(self):
+        return self.title
 
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.ForeignKey(Question)
-    author = models.ForeignKey(User)
+    question = models.ForeignKey(Question, default=0)
+    author = models.ForeignKey(User, default=0)
